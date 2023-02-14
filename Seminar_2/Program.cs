637d0bcb7262c107b22e570a0996f668cf69d771
yauhenia <yauhenia13@gmail.com>
2023-02-14 15:40:26 +0300

    Added Seminar 2

---
+++ Seminar_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// int CutNumber(int num)
+// {
+//     int hundreds = num / 100;
+//     int units = num % 10;
+//     int result = hundreds * 10 + units;
+
+//     return result;
+// }
+
+// int randNumber = new Random().Next(100,1000);
+
+// int newNumber = CutNumber(randNumber);
+// Console.WriteLine($"New version of {randNumber} is {newNumber}");
+
+// Задача 2.
+
+//bool IsMultiplyed(int num, int div1, int div2)
+// {
+//     return num % div1 == 0 && num % div2 == 0:
+// }
+
+// bool IsMultiplyed(int num, int div1, int div2)
+// {
+//     if( num % div1 == 0 && num % div2 ==0)
+//     // if( num % div1*div2 ==0)
+//         return true;
+//     else
+//         return false;
+// }
+

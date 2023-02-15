a8b412e7b0df65ea3d7445f31226d7084076edf4
yauhenia <yauhenia13@gmail.com>
2023-02-15 18:08:27 +0300

    Add HomeWork

---
+++ Homework_Seminar1/Program.cs
@@ -0,0 +1,103 @@
+﻿// HomeWork1.
+
+/* 
+Task 1.
+Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+// Console.Write("Input a first namber: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     Console.WriteLine("max = " + num1);
+// }
+// else
+// {
+//     Console.WriteLine("min = " + num1);
+// }
+
+// if(num1 < num2)
+// {
+// Console.WriteLine("max = " + num2);
+// }
+// else
+// {
+//     Console.WriteLine("min = " + num2);
+// }
+
+
+/* 
+Task 2.
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+// Console.Write("Input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a third number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if(num1 > max) max = num1;
+// if(num2 > max) max = num2;
+// if(num3 > max) max = num3;
+
+// Console.Write("max = " + max);
+
+/*
+Task 3.
+Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+// Console.Write("Input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int evenNum = num % 2;
+
+// if(evenNum == 0)
+// {
+//     Console.Write("Yes");
+// }
+// else
+// {
+//     Console.Write("No");
+// }
+
+/*
+Task 4.
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+// Console.Write("Input a namber: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int num = 1;
+
+// while(num <= number)
+// {
+//    if(num%2 == 0)
+//    {
+//     Console.Write(num + " ");
+//     num +=1;
+//    }
+// }
+
